7c55a89f0505118f064805c941a7dba6ff6a8f06
DiegoChew <diego.chew@gmail.com>
2024-03-16 22:17:37 -0600

    Integracion

--- Practicas/Practica1_modenas/Practrica_1.py
+++ Practicas/Practica1_modenas/Practrica_1.py
@@ -5,10 +5,17 @@ import pandas as pd
 from scipy import optimize as sco
 from scipy.special import comb
 import math
+# Título de la página
+st.title("Práctica 1- Distribución binomial en lanzamientos de monedas")
+ # Texto a mostrar
+texto = "Esta página tiene como fin graficar un histograma que muestre la distribución de conteos de caras de los primeros n tiros de 10 monedas, donde n va desde 1 a 100 tiros. Está diseñada de tal manera que se puede observar la distribución para cada caso respectivo"
+
+# Mostrar el texto
+st.write(texto)
 
 def binom(x, n, p):
-    x = np.asarray(x)  # Convertir a array si es necesario
-    n = int(n)
+    # x = np.asarray(x)  # Convertir a array si es necesario
+    # n = int(n)
     comb_vals = comb(n, x)
     p_x = p**x
     q_nx = (1-p)**(n-x)
@@ -18,11 +25,11 @@ sheet_id='1H943jgKOzqguRgjYEx7BPr5PoyvTXf7lfs9mqdlWBsk'
 
 df=pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv")
 
-n=st.slider("Ingrese la cantidad de intentos",1,100)
+m=st.slider("Ingrese la cantidad de intentos",1,100)
 
 
-# Seleccionar las primeras n filas y la columna "Diego - Saul"
-df_team = df.iloc[:n, :]["Diego - Saul"]
+# Seleccionar las primeras m filas y la columna "Diego - Saul"
+df_team = df.iloc[:m, :]["Diego - Saul"]
 
 # Calcular cuanto se repite cada valor
 repeat_counts = df_team.value_counts()
@@ -43,19 +50,61 @@ xD=np.array(repeat_counts.index)
 # print(xD)
 yD=np.array(repeat_counts.values)
 # print(yD)
-fit, cdds=sco.curve_fit(binom,repeat_counts_index.index,repeat_counts_index.values,p0=[np.mean(xD), np.mean(yD)/n], bounds=([0, 0], [100, 1]))
+fit, cdds=sco.curve_fit(binom,repeat_counts_index.index,repeat_counts_index.values, [10,0.5], bounds=([0, 0], [100, 1]))
 
 n=fit[0]
 p=fit[1]
 
 
 binomial_plot=px.line(x=repeat_counts_index.index,y=binom(repeat_counts_index.index,n,p),title="Binomial ajustada")
-binomial_plot.add_bar(x=repeat_counts_index.index,y=repeat_counts_index.values/100, name="Lanzamientos experimentales", marker_color='#CA6F1E')
+binomial_plot.add_bar(x=repeat_counts_index.index,y=repeat_counts_index.values/m, name="Lanzamientos experimentales", marker_color='#CA6F1E')
 binomial_plot.update_layout(xaxis_title="Cantidad de caras por tiro", yaxis_title="Cantidad de veces que obtivimos el caso")
 
 st.plotly_chart(binomial_plot)
 
 
+# Agregue los datos de el fit.
+val_fit=[n,p]
+tb1=pd.DataFrame(val_fit, columns=["Datos del fit"])
+st.table(tb1)
+# Agregue un botón para desplegar la tabla de datos a plotear. 
+st.write('Para ver la tabla de datos graficados, pulse el botón que esta debajo de este texto')
+
+ver_tabla = st.checkbox("Mostrar/ocultar tabla")
+
+if ver_tabla:
+    # Mostrar el DataFrame si el botón está activado
+    st.dataframe(df_team)
+
+codigo_latex = r'''P(n ; k) = \binom{n}{k} \cdot p^k \cdot (1-p)^{n-k} '''
+def mostrar_vineta():
+    # Información e imágenes que deseas mostrar en la viñeta
+    # st.markdown("sobre la distribución binomial")
+    st.write("La distribución binomial como una distribución de probabilidad discreta, cuenta el número de éxitos de n ensayos con probabilidad fija 'p' independientes entre si. Estos ensayos son del tipo dicotómico, es decir, solo son posibles dos resultados, el éxito o fracaso. Naturalmente se define el fracaso como q=1-p; Notemos que la suma de probabilidades es igual a 1, p+q=1. La ecuación de la binomial viene dada por:")
+ # Mostrar código LaTeX
+    st.latex(codigo_latex)
+#     st.write(" Mediante esta gráfica puede observar el comportamiento de esta gráfica")
+#     st.markdown("""
+#     <style>
+#         .imagen {
+#             display: block;
+#             margin-left: auto;
+#             margin-right: auto;
+#         }
+#     </style>
+# """, unsafe_allow_html=True)
+
+#     st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Binomial_distribution_pmf.svg/325px-Binomial_distribution_pmf.svg.png", caption="Descripción de la imagen", width=400, output_format='PNG', use_column_width=True)
+
+viñeta_visible = False
+
+# Botón para mostrar/ocultar la viñeta
+if st.button("Presione acá para tener información acerca de la distribución binomial"):
+    viñeta_visible = not viñeta_visible
+
+if viñeta_visible:
+    mostrar_vineta()
 
 # print(fit)
+
 
