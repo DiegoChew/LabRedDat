6f8795d7a2fee00823977458a02f20fb16369994
DiegoChew <diego.chew@gmail.com>
2024-03-17 01:42:12 -0600

    Un poco mas

--- Practicas/Practica1_modenas/Practrica_1.py
+++ Practicas/Practica1_modenas/Practrica_1.py
@@ -5,76 +5,125 @@ import pandas as pd
 from scipy import optimize as sco
 from scipy.special import comb
 import math
-# Título de la página
-st.title("Práctica 1- Distribución binomial en lanzamientos de monedas")
- # Texto a mostrar
-texto = "Esta página tiene como fin graficar un histograma que muestre la distribución de conteos de caras de los primeros n tiros de 10 monedas, donde n va desde 1 a 100 tiros. Está diseñada de tal manera que se puede observar la distribución para cada caso respectivo"
-
-# Mostrar el texto
-st.write(texto)
 
 def binom(x, n, p):
-    # x = np.asarray(x)  # Convertir a array si es necesario
-    # n = int(n)
     comb_vals = comb(n, x)
     p_x = p**x
     q_nx = (1-p)**(n-x)
     return comb_vals * p_x * q_nx
+# Título de la página
+st.caption("Práctica 1")
+st.title("Distribución binomial en lanzamientos de monedas")
+ # Texto a mostrar
+st.header("Objetivo general")
+st.markdown("Corrobrar la relación de probabilidad en los lanzamientos de monedas con la distribución binomial.")
+st.subheader("Objetivos secundarios")
+st.markdown("Corrobrar la relación de probabilidad entre la cantidad de lanzamientos.")
+st.markdown("Corrobrar la necesidad de cantidad de datos para una mejor interpretación.")
+st.divider()
+# texto = "Esta página tiene como fin graficar un histograma que muestre la distribución de conteos de caras de los primeros n tiros de 10 monedas, donde n va desde 1 a 100 tiros. Está diseñada de tal manera que se puede observar la distribución para cada caso respectivo"
+
+# Mostrar el texto
+# st.write(texto)
+
+st.markdown("Para esta practica se consideró dos distribuciones de datos, para la primera tendremos 100 datos donde en cada dato se lanzó 10 monedas y se contabilizó la cantidad de caras obtenidas, "
+            "esta distribución se utilizará un deslizador que va contabilizando lo obtenido por cada tiro, encontrando su ajuste a la binomial, su desviación estándar por los datos medidos y por el ajuste encontrado"
+            ". Para la segunda distribución se consideró 600 datos en loa cuales se lanzaron 10 monedas y se realizará lo mismo que se atrató anteriormente con el detalle que no tendremos el deslizador.")
 
 sheet_id='1H943jgKOzqguRgjYEx7BPr5PoyvTXf7lfs9mqdlWBsk'
 
 df=pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv")
 
-m=st.slider("Ingrese la cantidad de intentos",1,100)
+on=st.toggle("Primera distribución")
+if on:
+    st.subheader("Primera distribución")
+    m=st.slider("Ingrese la cantidad de intentos que desee contabilizar",1,100)
 
 
 # Seleccionar las primeras m filas y la columna "Diego - Saul"
-df_team = df.iloc[:m, :]["Diego - Saul"]
+    df_team = df.iloc[:m, :]["Diego - Saul"]
 
 # Calcular cuanto se repite cada valor
-repeat_counts = df_team.value_counts()
+    repeat_counts = df_team.value_counts()
 
 #Crear un rango de 0 a 10 para agregarlos a la tabla
-todos_numeros = range(11)
+    todos_numeros = range(11)
 
 #Agregando los valores de los numeros que no aparecieron
-repeat_counts_index = repeat_counts.reindex(todos_numeros, fill_value=0)
+    repeat_counts_index = repeat_counts.reindex(todos_numeros, fill_value=0)
 
-#crear un dataframe para graficar  
-df_grafico = pd.DataFrame({'Cantidad de caras por tiro': repeat_counts_index.index, 'Cantidad de veces que obtivimos el caso': repeat_counts_index.values})
+    fit, cdds=sco.curve_fit(binom,repeat_counts_index.index,repeat_counts_index.values, [10,0.5], bounds=([0, 0], [100, 1]))
 
-#grafica de barras de la tabla de datos
-# st.bar_chart(df_grafico, x="Cantidad de caras por tiro", y="Cantidad de veces que obtivimos el caso", color='#CA6F1E')
-
-xD=np.array(repeat_counts.index)
-# print(xD)
-yD=np.array(repeat_counts.values)
-# print(yD)
-fit, cdds=sco.curve_fit(binom,repeat_counts_index.index,repeat_counts_index.values, [10,0.5], bounds=([0, 0], [100, 1]))
-
-n=fit[0]
-p=fit[1]
+    n=fit[0]
+    p=fit[1]
 
 
-binomial_plot=px.line(x=repeat_counts_index.index,y=binom(repeat_counts_index.index,n,p),title="Binomial ajustada")
-binomial_plot.add_bar(x=repeat_counts_index.index,y=repeat_counts_index.values/m, name="Lanzamientos experimentales", marker_color='#CA6F1E')
-binomial_plot.update_layout(xaxis_title="Cantidad de caras por tiro", yaxis_title="Cantidad de veces que obtivimos el caso")
+# print(n2)
+# print(p2)
+    listaDS=np.array(df_team)
 
-st.plotly_chart(binomial_plot)
+    promDS=np.mean(listaDS)
+    standardDS=np.std(listaDS)
 
+    binomial_plot=px.line(x=repeat_counts_index.index,y=binom(repeat_counts_index.index,n,p),title=f"Binomial ajustada para {m} tiros")
+    binomial_plot.add_bar(x=repeat_counts_index.index,y=repeat_counts_index.values/m, name="Lanzamientos", marker_color='#CA6F1E')
+    binomial_plot.update_layout(xaxis_title="Cantidad de caras por tiro", yaxis_title="Cantidad de veces que obtivimos el caso")
 
-# Agregue los datos de el fit.
-val_fit=[n,p]
-tb1=pd.DataFrame(val_fit, columns=["Datos del fit"])
-st.table(tb1)
-# Agregue un botón para desplegar la tabla de datos a plotear. 
-st.write('Para ver la tabla de datos graficados, pulse el botón que esta debajo de este texto')
+    st.plotly_chart(binomial_plot)
 
-ver_tabla = st.checkbox("Mostrar/ocultar tabla")
+    listbin_DS=np.array(binom([1,2,3,4,5,6,7,8,9,10],n,p))
+    binDS=np.std(listbin_DS*10)
+    print(listaDS)
+    print(listbin_DS*m)
+    print(repeat_counts.index)
+    st.metric(label="Cantidad de elementos por tiro [n] obtenido por el fit", value=np.around(n,3))
+    st.metric(label="Probabilidad de cada moneda obtenida [p] por el fit", value=np.around(p,3))
+    st.metric(label=f"Promedio calculado por {m} tiros", value=np.around(promDS,3))
+    st.metric(label=f"Desviación estandar calculada en {m} tiros de manera experimental", value=np.around(standardDS,3))
+    st.metric(label=f"Desviación estandar calculada en {m} tiros considerando n={np.around(n,2)} y p={np.around(p,2)} ", value=np.around(binDS,3))
 
-if ver_tabla:
+    ver_tabla = st.checkbox("Datos graficados en la primera distribución")
+
+    if ver_tabla:
     # Mostrar el DataFrame si el botón está activado
-    st.dataframe(df_team)
+        st.dataframe(df_team)
+
+on2=st.toggle("Segunda distribución")
+if on2:
+    columns=['Lobsang - Rebeca','Guillermo - Shawn','Diego - Saul','Giovanna - Mario','Dessiré - Fabricio','Jacobo - Cesar']
+    filter_columns=df[columns]
+
+    melted_df = filter_columns.melt(var_name='column_name', value_name='value')
+
+# Ordenar el DataFrame resultante
+    sorted_df = melted_df.sort_values(by=['column_name'])
+
+# print(sorted_df)
+
+    values_g=sorted_df['value']
+
+# print(values_g)
+
+    repeat_values= values_g.value_counts()
+
+# print(repeat_values)
+
+    sorted_repeat_values=repeat_values.sort_index()
+
+# print(sorted_repeat_values)
+
+    fit2, cdds2=sco.curve_fit(binom,sorted_repeat_values.index,sorted_repeat_values.values, [10,0.5], bounds=([0, 0], [100, 1]))
+
+    n2=fit2[0]
+    p2=fit2[1]
+    
+    binomial_plot2=px.line(x=sorted_repeat_values.index,y=binom(sorted_repeat_values.index,n2,p2),title="Binomial ajustada")
+    binomial_plot2.add_bar(x=sorted_repeat_values.index,y=sorted_repeat_values.values/600, name="Lanzamientos experimentales", marker_color='#CA6F1E')
+    binomial_plot2.update_layout(xaxis_title="Cantidad de caras por tiro", yaxis_title="Cantidad de veces que obtivimos el caso")
+
+    st.plotly_chart(binomial_plot2)
+
+
 
 codigo_latex = r'''P(n ; k) = \binom{n}{k} \cdot p^k \cdot (1-p)^{n-k} '''
 def mostrar_vineta():
